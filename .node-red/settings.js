852aa4bdca506e7c2e6d9fd4e85a0c120ed97809
nelson466 <nvcamcho@gmail.com>
2025-11-19 22:07:43 -0400

    Update settings.js

--- .node-red/settings.js
+++ .node-red/settings.js
@@ -4,22 +4,5 @@ module.exports = {
   flowFile: "flows.json",
   // Secret para el cifrado interno de credenciales de Node-RED, aqui no se aplica Hash a "miClaveSecreta123"
   // El valor de credentialSecret solo debe ser una cadena de texto segura
-  credentialSecret: "miClaveSecreta123",
-  // Configuración del acceso al editor/administración:
-  // Nota: Node-RED busca la propiedad `adminAuth` (no "authAdmin").
-  adminAuth: {
-    type: "credentials",
-    users: [
-      {
-        username: "admin",
-        // Hash bcrypt de la contraseña "Nrvana654321"
-        // Generado con bcrypt (cost 10) online desde https://bcrypt-generator.com
-        password: "$2a$10$N32Qx5P10Np90VFpquUlhOjBPTGNsC2UWRpmM06KXNybeqD38W01C",
-        permissions: "*"
-      }
-    ]
-  }
-  // (opcional) otras configuraciones mínimas que podrías tener:
-  // uiPort: process.env.PORT || 1880,
-  // adminUI: { /* ... */ }
+  credentialSecret: "miClaveSecreta123"
 };
