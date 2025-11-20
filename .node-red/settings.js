f66510f25c987d9a5cc20d5ac0d29e82682f0c6c
nelson466 <nvcamcho@gmail.com>
2025-11-20 15:37:05 -0400

    Update settings.js

--- .node-red/settings.js
+++ .node-red/settings.js
@@ -1,5 +1,8 @@
 // settings.js - configuración para Node-RED (lista para copiar/pegar)
 module.exports = {
+  // Forzar a Node-RED a usar el puerto que provee Replit y a escuchar en todas las interfaces
+  uiPort: process.env.PORT || 3000,
+  uiHost: "0.0.0.0",
   // Forzar que Node-RED use 'flows.json' en el userDir
   flowFile: "flows.json",
   // Secret para el cifrado interno de credenciales de Node-RED, aqui no se aplica Hash a "miClaveSecreta123"
